c60f37a80f4c457ffea157aa03a024b0e522af0c
Ondrej Carnogursky <ondrej.carnogursky@gmail.com>
2020-04-03 00:11:28 +0200

    first commit

---
+++ build/static/js/main.6e5bfb5f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpowl-carousel"]=this["webpackJsonpowl-carousel"]||[]).push([[0],[,,,function(e,n,t){e.exports=t(4)},function(e,n,t){"use strict";t.r(n);var o=t(0),l=t.n(o),r=t(2),c=function(){return l.a.createElement("div",null,"Hello")};t.n(r).a.render(l.a.createElement(c,null),document.body)}],[[3,1,2]]]);
+//# sourceMappingURL=main.6e5bfb5f.chunk.js.map
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const OwlCarousel = () => {
+  return (
+    <div>Hello</div>
+  );
+}
+
+ReactDOM.render(<OwlCarousel />, document.body);
