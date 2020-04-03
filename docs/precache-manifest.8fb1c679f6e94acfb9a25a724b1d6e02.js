b9c5fb953dec639cbf7aee31b4b70310a382878e
Ondrej Carnogursky <ondrej.carnogursky@gmail.com>
2020-04-03 07:42:22 +0200

    copy build to docs for publish

---
+++ docs/precache-manifest.8fb1c679f6e94acfb9a25a724b1d6e02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323eacfb20ffffacc0822f73a6f5fcfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dd3d918c97cd0137e15",
+    "url": "/static/js/2.eaa62161.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eaa62161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e8097e8200d989a3c55",
+    "url": "/static/js/main.6e5bfb5f.chunk.js"
+  },
+  {
+    "revision": "0055d6ff94d9d8366c2b",
+    "url": "/static/js/runtime-main.8a8875e8.js"
+  }
+]);
